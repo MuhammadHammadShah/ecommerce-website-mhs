fcd81229f946c5a1003c57cce8f077020b7bc047
Muhammad Hammad Shah <Hammad4455shah5544@gmail.com>
2023-08-19 19:06:10 +0500

    images are changed and some changes occured now going to cart functinality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { client } from "@/lib/sanityClient";
 import { Image as IImage } from "sanity";
-
+import Image from "next/image";
+import { urlForImage } from "../../sanity/lib/image";
 interface IProduct {
   title: string;
   description: string;
@@ -32,11 +33,21 @@ const Home = async () => {
   console.log(data);
 
   return (
-    <div>
+    <div className="grid grid-cols-[repeat(3,auto)] justify-center gap-x-10">
       {data.map((item) => (
         <div>
+          <Image
+            width={200}
+            height={200}
+            alt="images"
+            className="max-h-[200px] object-cover object-top"
+            src={urlForImage(item.image[0] as IImage).url()}
+          />
           <h1>{item.title}</h1>
-          <h2>{item.description}</h2>
+          <h3>${item.price}</h3>
+          <button className="border py-2 px-6 rounded bg-blue-600 text-white">
+            Add to cart
+          </button>
         </div>
       ))}
     </div>
